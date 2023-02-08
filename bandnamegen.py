f748bcb2e9371c60f8a9d72f442e07a4f35ba36c
v0raz <122883606+v0raz@users.noreply.github.com>
2023-02-08 10:27:28 -0300

    Update bandnamegen.py

--- bandnamegen.py
+++ bandnamegen.py
@@ -1,4 +1,4 @@
-city_name = input("What's the name of the city you grew in? ")
-pet_name = input("What's the name of your first pet? ")
+city_name = input("What's the name of the city you grew in?\n")
+pet_name = input("What's the name of your first pet?\n")
 band_name = (f"Your band name is {city_name} {pet_name}")
 print (band_name)
